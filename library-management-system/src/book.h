68ccc9af30277d078c629730cd6142b863770cbf
govardhankummarig-web <govardhankummarig@gmail.com>
2026-05-22 07:52:26 +0530

    Add book management function declarations

--- library-management-system/src/book.h
+++ library-management-system/src/book.h
@@ -1,1 +1,17 @@
+#ifndef BOOK_H
+#define BOOK_H
 
+#include "library.h"
+
+// Book Management Functions
+void addNewBook();
+void updateBookDetails();
+void removeBook();
+void showAllBooks();
+
+// Helper Functions
+int findBookIndexById(int bookId);
+int isBookAvailable(int bookId);
+void saveBooksToFile();
+
+#endif
