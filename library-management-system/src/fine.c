68655b796d6720be550d248cc5248c830162b1c3
shrejal12389 <srejala755@gmail.com>
2026-05-26 14:57:29 +0530

    .

--- library-management-system/src/fine.c
+++ library-management-system/src/fine.c
@@ -1,19 +1,98 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "fine.h"
 
-int calculateFine(int dueDay, int returnDay) {
+// External Variables
+extern IssueRecord* issues;
+extern int issueCount;
 
-    int lateDays;
-    int fine;
+extern Book* books;
+extern int bookCount;
 
-    if(returnDay <= dueDay) {
+extern Member* members;
+extern int memberCount;
 
-        return 0;
+void generateMemberReport() {
+
+    printf("\n===== MEMBER REPORT =====\n");
+
+    if(memberCount == 0) {
+
+        printf("No Members Registered.\n");
+        return;
     }
 
-    lateDays = returnDay - dueDay;
+    for(int i = 0; i < memberCount; i++) {
 
-    fine = lateDays * 2;
+        double totalFine = 0;
 
-    return fine;
+        for(int j = 0; j < issueCount; j++) {
+
+            if(issues[j].memberId ==
+               members[i].id) {
+
+                totalFine += issues[j].fine;
+            }
+        }
+
+        printf("\nMember ID   : %d\n",
+               members[i].id);
+
+        printf("Name        : %s\n",
+               members[i].name);
+
+        printf("Email       : %s\n",
+               members[i].email);
+
+        printf("Total Fine  : Rs. %.0f\n",
+               totalFine);
+
+        printf("----------------------------\n");
+    }
+}
+
+void generateFineReport() {
+
+    printf("\n===== FINE REPORT =====\n");
+
+    int found = 0;
+
+    for(int i = 0; i < issueCount; i++) {
+
+        if(issues[i].fine > 0) {
+
+            int bookIndex =
+                findBookIndexById(
+                    issues[i].bookId
+                );
+
+            int memberIndex =
+                findMemberIndexById(
+                    issues[i].memberId
+                );
+
+            printf("\nIssue ID    : %d\n",
+                   issues[i].id);
+
+            printf("Book        : %s\n",
+                   books[bookIndex].title);
+
+            printf("Member      : %s\n",
+                   members[memberIndex].name);
+
+            printf("Fine Amount : Rs. %.0f\n",
+                   issues[i].fine);
+
+            printf("-------------------------\n");
+
+            found = 1;
+        }
+    }
+
+    if(!found) {
+
+        printf("No Fine Records Found.\n");
+    }
 }
